5e5f69008790a49b5cc5154907f39e833f565f9d
aklezovics <arturs.klezovics@raisin.com>
2021-03-18 17:07:07 +0100

    Add functionality for creating test suites

---
+++ src/test/java/com/klezovich/springtestplayground/suites/processor/ProcessorSlowTest.java
@@ -0,0 +1,15 @@
+package com.klezovich.springtestplayground.suites.processor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+@Tag("slow")
+public class ProcessorSlowTest {
+
+    @Test
+    void testProcessorSlow() {
+        log.info("Running SLOW processor test");
+    }
+}
