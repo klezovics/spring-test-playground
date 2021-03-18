76b07341a4e45eb83174814376e8b0dcfc672af9
aklezovics <arturs.klezovics@raisin.com>
2021-03-18 16:00:30 +0100

    Initial commit

---
+++ src/main/java/com/klezovich/springtestplayground/SpringTestPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.klezovich.springtestplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/klezovich/springtestplayground/SpringTestPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klezovich.springtestplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
